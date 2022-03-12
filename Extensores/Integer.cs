d9f2a4b6e882e082f8ecd1c70718bd911769e03f
Saúl <saulvaldelvira@gmail.com>
2022-03-12 12:50:49 +0100

    Agregar archivos de proyecto.

---
+++ Extensores/Integer.cs
@@ -0,0 +1,20 @@
+﻿namespace Extensores {
+    public static class Integer {
+        public static bool IsPrime(this int n) {
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+
+        public static int NextPrime(this int n) {
+            while (!(++n).IsPrime());
+            return n;
+        }
+
+        public static int PreviousPrime(this int n) {
+            while (!(--n).IsPrime());
+            return n;
+        }
+    }
+}
---
+++ Extensores/String.cs
@@ -0,0 +1,19 @@
+﻿namespace Extensores {
+    public static class String {
+        public static int NumeroVocales(this string str) {
+            int result = 0;
+            foreach (char c in str)
+                if ("aeiouáéíóú".Contains(c))
+                    result++;
+            return result;
+        }
+
+        public static int CuentaCaracter(this string str, char ch) {
+            int result = 0;
+            foreach (char c in str)
+                if (ch == c)
+                    result++;
+            return result;
+        }
+    }
+}
